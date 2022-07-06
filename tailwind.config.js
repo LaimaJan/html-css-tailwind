78d0c8a91909814f5c9c0f394d12dbc002f284f1
LaimaJan <lm.lingyte@gmail.com>
2022-07-06 17:53:58 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+// const colors = require("tailwindscss/colors");
+
+module.exports = {
+	content: ["./src/*.{html,js}", "./src/**/*.{html,js}"],
+	theme: {
+		extend: {},
+	},
+	// colors: {
+	// 	green: "#7ABCAC",
+	// 	yellow: "#D7DA79",
+	// 	red: "#C29485",
+	// },
+	plugins: [],
+};
